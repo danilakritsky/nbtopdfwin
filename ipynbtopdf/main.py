20dfa6e733fbede8dbe91d0356a2a69ae1eca98e
Danila Kritsky <danilakritsky@gmail.com>
2023-01-18 11:51:51 +0300

    Add save_outputs function and tests

--- ipynbtopdf/main.py
+++ ipynbtopdf/main.py
@@ -4,6 +4,7 @@ import nbformat
 from nbconvert import LatexExporter
 
 def notebook_to_latex(notebook: str | Path) -> tuple[str, dict]:
+    "Convert notebook to latex and return its content and additional data."
     notebook_node = nbformat.read(
         notebook,
         as_version=4
@@ -14,3 +15,11 @@ def notebook_to_latex(notebook: str | Path) -> tuple[str, dict]:
 
     latex_body, resources = exporter.from_notebook_node(notebook_node)
     return latex_body, resources
+
+
+def save_outputs(outputs: dict[str, bytes], dir: str | Path = '.') -> None:
+    "Save each output to a file."
+    cur_output: str
+    for cur_output in outputs:
+        with open(Path(dir) / cur_output, 'wb') as f:
+            f.write(outputs[cur_output])
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,10 +5,26 @@ import pytest
 from ipynbtopdf import main
 
 @pytest.fixture
-def test_file():
-    return Path(__file__).parent / 'test_file.ipynb'
+def test_folder():
+    return Path(__file__).parent
+
+
+@pytest.fixture
+def test_file(test_folder):
+    return test_folder / 'test_file.ipynb'
+
 
 def test_notebook_to_latex(test_file):
     body, resources = main.notebook_to_latex(test_file)
     assert "Привет" in body
     assert resources['outputs']
+
+
+def test_save_outputs(test_folder, test_file):
+    body, resources = main.notebook_to_latex(test_file)
+    main.save_outputs(resources['outputs'], dir=test_folder)
+    
+    output: str
+    for output in resources['outputs']:
+        assert (test_folder  / output).exists
+        (test_folder  / output).unlink()
